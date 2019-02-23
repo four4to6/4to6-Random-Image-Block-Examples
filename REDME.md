dbd21f8ae8607e0d5e91fde039b9a598b16abf2e
four4to6 <yu-yo@kha.biglobe.ne.jp>
2019-02-24 04:37:21 +0900

    no message

---
+++ REDME.md
@@ -0,0 +1,55 @@
+4to6-Random-Image-Block-Examples
+===
+4to6-Random-Image-Block-Examples は、
+WordPress ブロックエディタ (Gutenberg) 用の
+カスタマイズ ブロック example です。
+
+## Description
+
+あくまで beginner が対象です。
+カスタムブロックをCreateしてみたいとか、
+Tryしてみたいという方のTips and Referenceになれば幸いです。
+
+note: It is not secure because the image source is non-ssl communication.
+
+## Usage
+
+WordPress plug-inとしてインストールしてください。
+dashboardからのインストールか、ファイルをuploadして
+インストールして頂く事で、Custom Blockとしてお使い頂けます。
+
+## Install
+
+4to6-Random-Image-Block-Examples-master.zip をdownloadしたら  
+以下に従いplug-inとしてインストールして下さい。
+1. WordPress管理ダッシュボードの [ **プラグイン** ] > [ **新規追加** ] から  
+[ **プラグインを追加** ] に移動します。
+1. ページ上部の [ **プラグインのアップロード** ] をクリックします。
+1. [ **参照...** ] をクリックし、ダウンロードしたzipファイルを見つけてアップロードします。
+1. プラグインのインストールが完了したら、[ **有効にする** ] をクリックします。
+1. You’re done!
+
+## Licence
+
+License : GNU General Public License
+
+License URI : [https://github.com/WordPress/gutenberg/blob/master/LICENSE.md](https://github.com/WordPress/gutenberg/blob/master/LICENSE.md) 
+
+GNU Public License : [https://wordpress.org/about/license/](https://wordpress.org/about/license/) 
+
+
+## Author
+
+Customized it : [FOUR 4to6](https://github.com/four4to6) 
+
+## References
+
+- **The underlying block**
+  - [https://github.com/WordPress/gutenberg/tree/master/packages/blocks](https://github.com/WordPress/gutenberg/tree/master/packages/blocks)
+- **Gutenberg Handbook**
+  - [https://wordpress.org/gutenberg/handbook/](https://wordpress.org/gutenberg/handbook/)
+- **Dashicons**
+  - [https://developer.wordpress.org/resource/dashicons/](https://developer.wordpress.org/resource/dashicons/) 
+- **GitHub Flavored Markdown**
+  - [https://guides.github.com/features/mastering-markdown/](https://guides.github.com/features/mastering-markdown/)
+  - [https://help.github.com/articles/basic-writing-and-formatting-syntax/](https://help.github.com/articles/basic-writing-and-formatting-syntax/)
---
+++ block.js
@@ -0,0 +1,81 @@
+﻿// block.js
+( function( blocks, editor, i18n, element ) {
+	var el = element.createElement,
+		source = blocks.source;
+	var __ = i18n.__;
+	var RichText = editor.RichText;
+
+	function RandomImage( props ) {
+		var src = 'http://lorempixel.com/400/200/' + props.category;
+
+		return el( 'img', {
+			src: src,
+			alt: props.category
+		} );
+	}
+
+	blocks.registerBlockType( 'myplugin/random-image', {
+		title: 'Random Image',
+
+		// icon: 'format-image',   // Specifying a dashicon for the block
+		icon: ( { background:'#ffa5a5', foreground:'#333333', src:'format-image',} ),
+
+		category: 'common',
+
+                description: __( 'Block displaying random images.' ),
+
+                // Make it easier to discover a block with keyword aliases.
+                // These can be localised so your keywords work across locales.
+                keywords: [ __( 'image' ), __( 'photo' ), __( 'random' ) ],
+
+                styles: [ { isDefault: false }, ],
+
+		attributes: {
+			category: {
+				type: 'string',
+				source: 'attribute',
+				attribute: 'alt',
+				selector: 'img',
+			}
+		},
+
+		edit: function( props ) {
+
+			var category = props.attributes.category,
+				children;
+
+			function setCategory( event ) {
+				var selected = event.target.querySelector( 'option:checked' );
+				props.setAttributes( { category: selected.value } );
+				event.preventDefault();
+			}
+
+			children = [];
+			if ( category ) {
+				children.push( RandomImage( { category: category } ) );
+			}
+
+			children.push(
+				el( 'select', { value: category, onChange: setCategory },
+					el( 'option', null, '- Select -' ),
+					el( 'option', { value: 'cats' }, 'Cats' ),
+					el( 'option', { value: 'fashion' }, 'Fashion' ),
+					el( 'option', { value: 'animals' }, 'Animals' ),
+					el( 'option', { value: 'city' }, 'City' ),
+					el( 'option', { value: 'nature' }, 'Nature' )
+				)
+			);
+
+			return el( 'form', { onSubmit: setCategory }, children );
+		},
+
+		save: function( props ) {
+			return RandomImage( { category: props.attributes.category } );
+		}
+	} );
+} )(
+	window.wp.blocks,
+	window.wp.editor,
+	window.wp.i18n,
+	window.wp.element,
+);
